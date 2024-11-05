7be571e7524222d087264f887685cdef0b73134f
Dallas McCall <dallas@dmos.tools>
2024-11-05 15:19:14 -0500

    Reverts manual patch

--- lib/src/editor/util/platform_extension.dart
+++ lib/src/editor/util/platform_extension.dart
@@ -12,22 +12,6 @@ extension PlatformExtension on Platform {
   /// Returns true if the operating system is macOS and not running on Web platform.
   static bool get isMacOS => UniversalPlatform.isMacOS;
 
-  /// Returns true if the operating system is IOS and not running on Web platform.
-  static bool get isIOS {
-    if (kIsWeb) {
-      return false;
-    }
-    return Platform.isIOS;
-  }
-
-  /// Returns true if the operating system is Android and not running on Web platform.
-  static bool get isAndroid {
-    if (kIsWeb) {
-      return false;
-    }
-    return Platform.isAndroid;
-  }
-
   /// Returns true if the operating system is Windows and not running on Web platform.
   static bool get isWindows => UniversalPlatform.isWindows;
 
